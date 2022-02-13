252cdcd77338881993f4a984bb0f6d01c54ccc37
Samir Qatim <sqatim@student.1337.ma>
2022-02-13 16:23:46 +0100

    feat: add authentication

---
+++ src/app.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
+
+// var samir = (): any => {console.log("lalalalalalal")};
+
+@Controller()
+export class AppController {
+  constructor(private authService: AuthService) {}
+
+  // @samir()
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+}
